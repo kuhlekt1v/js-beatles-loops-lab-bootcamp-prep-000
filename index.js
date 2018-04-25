e0f254714242fb95991ad14f4e58fcd39e6019e0
Cody Sheridan <csheridan994@gmail.com>
2018-04-25 10:14:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ function johnLennonFacts(a) {
   return facts;
 }
 
-function iLoveTheBeatles(number){
+function iLoveTheBeatles(n){
   var array = [];
   do {
      array.push("I love the Beatles!");
-     i++;
-  } while (i < 15);
+     n++;
+  } while (n < 15);
     return array;
 }
