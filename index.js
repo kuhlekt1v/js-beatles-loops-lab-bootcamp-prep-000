43353d7354eb1009d199b45a4652c17247d98b99
Cody Sheridan <csheridan994@gmail.com>
2018-04-25 09:39:15 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,30 @@
+//var musician = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+//var instrument = ["guitar", "bass", "lead guitar", "drums"];
+
+function theBeatlesPlay(musician, instrument) {
+  var array = [];
+  for (let i = 0; i < musician.length; i++) {
+    array.push(`${musician[i]} plays ${instrument[i]}`);
+  }
+  return array;
+}
+
+function johnLennonFacts(a) {
+  var facts = [];
+  let i = 0;
+  while (i < a.length) {
+    facts.push(`${a[i]}!!!`);
+    i++;
+  }
+  return facts;
+}
+
+function iLoveTheBeatles(number) {
+  var array = [];
+  let i = 0;
+  do {
+    i++;
+    return i;
+  } while (array.length <= 7 );
+  return array;
+}
